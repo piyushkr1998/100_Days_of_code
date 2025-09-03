c4fcb373e0461fbae1ee79298b532252a5ce431d
piyushkr1998 <piyushkr6722@gmail.com>
2025-09-03 23:34:44 +0530

    solved first Q

--- Day1/Q1.c
+++ Day1/Q1.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-int main (){
-printf("Hello world");
+int main() {
+int a,b;
+printf("first number");
+scanf("%d", &a);
+
+printf("second number");
+scanf("%d",&b);
+
+int sum = a+b;
+
+printf("sum of the two numbers:%d", sum);
 
 return 0;
 }
