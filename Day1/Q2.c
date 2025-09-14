cb10e28018ed51d3e4881f75e41332d340818e8e
piyushkr1998 <piyushkr6722@gmail.com>
2025-09-14 15:02:04 +0530

    dfjv

--- Day1/Q2.c
+++ Day1/Q2.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
-int main () {
+int main(){
+    int a,b,sum,product,quo;
+    printf("Enter any number a:");
+    scanf("%d", &a);
 
-    printf("Hello world\n");
-    return 0;
+    printf("Enter any number b:");
+    scanf("%d",&b);
+
+    product= a*b;
+     printf("product is %d\n",product);
+
+     sum=a+b;
+     printf("Sum is %d\n ",sum);
+
+     quo=a/b;
+     printf("quo is %d\n",quo);
+
+     return 0;
+
+
 }
