dfd1ebe68c850643456b03343055b93c74e859a1
piyushkr1998 <piyushkr6722@gmail.com>
2025-09-15 01:27:54 +0530

    hii

---
+++ Day4/Q8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int n , sum ;
+    printf("Enter any natural number n :");
+    scanf("%d",&n);
+
+    sum = n * (n+1)/2;
+    printf("Sum of first natural number is %d",sum);
+
+    
+
+    return 0;
+
+
+}
